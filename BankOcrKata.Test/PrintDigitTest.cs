33fe63aeab895b188453c2a536b016a1928a5195
Erik Dietrich <erik@daedtech.com>
2014-05-15 14:24:27 -0500

    Logic in place for 0 - 9 PrintedDigits; handles 3x3 things that don't match; refactored to be more declarative in code using Linq

--- BankOcrKata.Test/PrintDigitTest.cs
+++ BankOcrKata.Test/PrintDigitTest.cs
@@ -14,6 +14,26 @@ namespace BankOcrKata.Test
         public class IntegerValue
         {
             [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Returns_Negative_1_When_Passed_Charcters_Not_Matching_A_Number()
+            {
+                var linesForGarbage = new string[] { "   ", " _ ", "   " };
+
+                var digit = new PrintedDigit(linesForGarbage);
+
+                Assert.AreEqual<int>(-1, digit.IntegerValue);
+            }
+
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Returns_0_When_Passed_Characters_For_Zero()
+            {
+                var linesForPrintedZero = PrintedDigit.PrintedIntegerDefinitions[0];
+
+                var digit = new PrintedDigit(linesForPrintedZero);
+
+                Assert.AreEqual<int>(0, digit.IntegerValue);
+            }
+
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
             public void Returns_1_When_Passed_Characters_For_1()
             {
                 var linesForPrintedOne = PrintedDigit.PrintedIntegerDefinitions[1];
@@ -44,9 +64,65 @@ namespace BankOcrKata.Test
             }
 
             [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
-            public void Returns_4_WHen_Passed_Characters_For_4()
+            public void Returns_4_When_Passed_Characters_For_4()
             {
-                
+                var linesForPrintedFour = PrintedDigit.PrintedIntegerDefinitions[4];
+
+                var digit = new PrintedDigit(linesForPrintedFour);
+
+                Assert.AreEqual<int>(4, digit.IntegerValue);
+
+            }
+
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Returns_5_When_Passed_Characters_For_5()
+            {
+                var linesForPrintedFive = PrintedDigit.PrintedIntegerDefinitions[5];
+
+                var digit = new PrintedDigit(linesForPrintedFive);
+
+                Assert.AreEqual<int>(5, digit.IntegerValue);
+
+            }
+
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Returns_6_When_Passed_Characters_For_6()
+            {
+                var linesForPrintedSix = PrintedDigit.PrintedIntegerDefinitions[6];
+
+                var digit = new PrintedDigit(linesForPrintedSix);
+
+                Assert.AreEqual<int>(6, digit.IntegerValue);
+            }
+
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Returns_7_When_Passed_Characters_For_7()
+            {
+                var linesForPrintedSeven = PrintedDigit.PrintedIntegerDefinitions[7];
+
+                var digit = new PrintedDigit(linesForPrintedSeven);
+
+                Assert.AreEqual<int>(7, digit.IntegerValue);
+            }
+
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Returns_8_When_Passed_Characters_For_8()
+            {
+                var linesForPrintedEight = PrintedDigit.PrintedIntegerDefinitions[8];
+
+                var digit = new PrintedDigit(linesForPrintedEight);
+
+                Assert.AreEqual<int>(8, digit.IntegerValue);
+            }
+
+            [TestMethod, Owner("ebd"), TestCategory("Proven"), TestCategory("Unit")]
+            public void Returns_9_When_Passed_Characters_For_9()
+            {
+                var linesForPrintedNine = PrintedDigit.PrintedIntegerDefinitions[9];
+
+                var digit = new PrintedDigit(linesForPrintedNine);
+
+                Assert.AreEqual<int>(9, digit.IntegerValue);
             }
         }
     }
@@ -55,8 +131,12 @@ namespace BankOcrKata.Test
     {
         public static readonly string[][] PrintedIntegerDefinitions = new string[][]
         {
-            new string[]{ "", "", ""},
-            new string[]{ 
+            new string[] {
+                " _ ",
+                "| |",
+                "|_|"
+            },
+            new string[] { 
                 "   ", 
                 "  |",  
                 "  |" 
@@ -66,28 +146,58 @@ namespace BankOcrKata.Test
                 " _|",  
                 "|_ " 
             },
-            new string[]{ 
+            new string[] { 
                 " _ ",
                 " _|", 
                 " _|" 
+            },
+            new string[] {
+                "   ",                   
+                "|_|",
+                "  |"
+            },
+            new string[] {
+                " _ ",
+                "|_ ",
+                " _|",
+            },
+            new string[] {
+                " _ ",
+                "|_ ",
+                "|_|"
+            },
+            new string[] {
+                " _ ",
+                "  |",
+                "  |",
+            },
+            new string[] {
+                " _ ",
+                "|_|",
+                "|_|"
+            },
+            new string[] {
+                " _ ",
+                "|_|",
+                " _|"
             }
         };
 
         public int IntegerValue { get; private set; }
 
-        public PrintedDigit(IList<string> lines)
+        public PrintedDigit(IList<string> inputRows)
         {
-            for (int index = 1; index < 4; index++)
-            {
-                if (MatchesEntryInDefinitions(index, lines))
-                    IntegerValue = index;
-            }
+            var possibleDigits = Enumerable.Range(0, 10);
+            var possibleDigitsAsNullables = possibleDigits.Select(i => new int?(i));
+            var digitThatMatchesOrNull = possibleDigitsAsNullables.FirstOrDefault(nullableDigit => MatchesDefinedMatrixFor(nullableDigit.Value, inputRows));
+            IntegerValue = digitThatMatchesOrNull ?? -1;
         }
-        private bool MatchesEntryInDefinitions(int integerToTry, IList<string> lines)
+
+        private static bool MatchesDefinedMatrixFor(int digitToTry, IList<string> inputRows)
         {
-            return PrintedIntegerDefinitions[integerToTry][0] == lines[0]
-                && PrintedIntegerDefinitions[integerToTry][1] == lines[1]
-                && PrintedIntegerDefinitions[integerToTry][2] == lines[2];
+            var actualMatrixForDigitToTry = PrintedIntegerDefinitions[digitToTry];
+            var rowIndeces = Enumerable.Range(0, actualMatrixForDigitToTry.Length);
+            return rowIndeces.All(i => actualMatrixForDigitToTry[i] == inputRows[i]);
         }
     }
 }
