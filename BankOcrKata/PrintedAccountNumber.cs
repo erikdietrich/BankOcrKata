459d779ae828698903ae840a8c263efb7958de8b
Erik Dietrich <erik@daedtech.com>
2014-05-15 18:29:42 -0500

    Created a pretty intense Linq one liner, and while I'm happy with the declarative nature of this and wanted to save it for later reference, this is inredibly dense and unreadable.

--- BankOcrKata/PrintedAccountNumber.cs
+++ BankOcrKata/PrintedAccountNumber.cs
@@ -15,18 +15,13 @@ namespace BankOcrKata
             if (accountNumberLines.Length != 3)
                 throw new BadAccountNumberFormatException();
 
-            var printDigits = new List<PrintedDigit>();
-            for (int index = 0; index < 9; index++)
-            {
-                var lines = new string[]
-                {
-                    accountNumberLines[0].Substring(index * 3, 3),
-                    accountNumberLines[1].Substring(index * 3, 3),
-                    accountNumberLines[2].Substring(index * 3, 3)
-                };
-                printDigits.Add(new PrintedDigit(lines));
-            }
+            var printDigits = ParseAccountNumberIntoDigits(accountNumberLines);
             DisplayValue = String.Join(string.Empty, printDigits.Select(pd => pd.IntegerValue));
+        }
+
+        private static List<PrintedDigit> ParseAccountNumberIntoDigits(string[] accountNumberLines)
+        {
+            return Enumerable.Range(0, 9).Select(digitNumber => new PrintedDigit(Enumerable.Range(0, 3).Select(row => accountNumberLines[row].Substring(digitNumber * 3, 3)).ToArray())).ToList();
         }
     }
 }
